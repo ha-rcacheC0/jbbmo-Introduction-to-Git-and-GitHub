e3d1c38e229aa0c4f9283ff39ce599634a7c6b7d
Lucas Keller <148141760+ha-rcacheC0@users.noreply.github.com>
2024-12-17 18:48:02 -0600

    made changes to right branch

--- simple-interest.sh
+++ simple-interest.sh
@@ -3,8 +3,8 @@
 # Do not use this in production. Sample purpose only.
 
 # Author: Upkar Lidder (IBM)
-# Additonal Authors:
-# ha-rcache-C0
+## Additonal Authors:
+## ha-rcache-C0
 
 # Input:
 # p, principal amount
